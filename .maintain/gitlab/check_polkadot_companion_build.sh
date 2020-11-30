9687966e79fe70e4c76159ec1e60266719d6ed1c
transxask <orwx71@163.com>
2020-11-30 17:37:18 +0800

    迁移代码到substrate2.0

--- .maintain/gitlab/check_polkadot_companion_build.sh
+++ .maintain/gitlab/check_polkadot_companion_build.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env sh
 #
-# check if a pr is compatible with polkadot companion pr or master if not 
+# check if a pr is compatible with polkadot companion pr or master if not
 # available
 #
-# to override one that was just mentioned mark companion pr in the body of the 
+# to override one that was just mentioned mark companion pr in the body of the
 # polkadot pr like
 #
 # polkadot companion: paritytech/polkadot#567
@@ -12,7 +12,7 @@
 
 github_api_substrate_pull_url="https://api.github.com/repos/paritytech/substrate/pulls"
 # use github api v3 in order to access the data without authentication
-github_header="Authorization: token ${GITHUB_PR_TOKEN}" 
+github_header="Authorization: token ${GITHUB_PR_TOKEN}"
 
 boldprint () { printf "|\n| \033[1m${@}\033[0m\n|\n" ; }
 boldcat () { printf "|\n"; while read l; do printf "| \033[1m${l}\033[0m\n"; done; printf "|\n" ; }
@@ -31,10 +31,7 @@ polkadot companion: paritytech/polkadot#567
 
 
 it will then run cargo check from this polkadot's branch with substrate code
-from this pull request. in absence of that string it will check if a polkadot
-pr is mentioned and will use the last one instead. if none of the above can be
-found it will check if polkadot has a branch of the exact same name than the
-substrate's branch. if it can't find anything, it will uses master instead
+from this pull request. otherwise, it will uses master instead
 
 
 EOT
@@ -43,14 +40,16 @@ EOT
 git config --global user.name 'CI system'
 git config --global user.email '<>'
 
-SUBSTRATE_PATH=$(pwd)
+cargo install -f --version 0.2.0 diener
 
 # Merge master into our branch before building Polkadot to make sure we don't miss
 # any commits that are required by Polkadot.
 git merge origin/master
 
 # Clone the current Polkadot master branch into ./polkadot.
-git clone --depth 1 https://github.com/paritytech/polkadot.git
+# NOTE: we need to pull enough commits to be able to find a common
+# ancestor for successfully performing merges below.
+git clone --depth 20 https://github.com/paritytech/polkadot.git
 
 cd polkadot
 
@@ -72,14 +71,6 @@ then
       -e 's;^.*polkadot companion: https://github.com/paritytech/polkadot/pull/([0-9]+).*$;\1;p' \
     | tail -n 1)"
 
-  if [ -z "${pr_companion}" ]
-  then
-    pr_companion="$(echo "${pr_body}" | sed -n -r \
-      -e 's;^.*paritytech/polkadot/#([0-9]+).*$;\1;p' \
-      -e 's;^.*https://github.com/paritytech/polkadot/pull/([0-9]+).*$;\1;p' \
-      | tail -n 1)"
-  fi
-
   if [ "${pr_companion}" ]
   then
     boldprint "companion pr specified/detected: #${pr_companion}"
@@ -87,33 +78,17 @@ then
     git checkout pr/${pr_companion}
     git merge origin/master
   else
-    pr_ref="$(grep -Po '"ref"\s*:\s*"\K(?!master)[^"]*' "${pr_data_file}")"
-    if git fetch origin "$pr_ref":branch/"$pr_ref" 2>/dev/null
-    then
-      boldprint "companion branch detected: $pr_ref"
-      git checkout branch/"$pr_ref"
-      git merge origin/master
-    else
-      boldprint "no companion branch found - building polkadot:master"
-    fi
+    boldprint "no companion branch found - building polkadot:master"
   fi
   rm -f "${pr_data_file}"
 else
   boldprint "this is not a pull request - building polkadot:master"
 fi
 
-# Make sure we override the crates in native and wasm build
-# patching the git path as described in the link below did not test correctly
-# https://doc.rust-lang.org/cargo/reference/overriding-dependencies.html
-mkdir .cargo
-echo "paths = [ \"$SUBSTRATE_PATH\" ]" > .cargo/config
-
-mkdir -p target/debug/wbuild/.cargo
-cp .cargo/config target/debug/wbuild/.cargo/config
-
-# package, others are updated along the way.
-cargo update
+cd ..
+$CARGO_HOME/bin/diener --substrate --branch $CI_COMMIT_REF_NAME --git https://gitlab.parity.io/parity/substrate.git --path polkadot
+cd polkadot
 
 # Test Polkadot pr or master branch with this Substrate commit.
+cargo update -p sp-io
 time cargo test --all --release --verbose
-
